1834ad2152a02c879864b3f422d5f001b0890561
Jiahua Cui <jiahua@brainboxlabs.com>
2024-08-07 09:50:25 -0400

    added users route and user html heex and modified original home page

--- lib/phx_web/controllers/page_controller.ex
+++ lib/phx_web/controllers/page_controller.ex
@@ -6,4 +6,10 @@ defmodule PhxWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :users, layout: false)
+  end
 end
