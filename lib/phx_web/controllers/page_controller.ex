03b13c69de8152c1971825aa80d3519823bdcd05
Jiahua Cui <jiahua@brainboxlabs.com>
2024-08-08 09:34:10 -0400

    updated users template to loop through users, and updated pagecontroller to return user in json format

--- lib/phx_web/controllers/page_controller.ex
+++ lib/phx_web/controllers/page_controller.ex
@@ -10,6 +10,13 @@ defmodule PhxWeb.PageController do
   def users(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :users, layout: false)
+    users = [
+      %{id: 1, name: "alice", email: "alice@email.com"},
+      %{id: 2, name: "bob", email: "bob@email.com"},
+    ]
+    # the below code renders the page
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+
   end
 end
